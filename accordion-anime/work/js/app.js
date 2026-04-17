e7eb1f87d0bc4da9c35ccac600510e8c6caa4e23
SuzuneSAKAKIBARA <suzunesakakibara@gmail.com>
2026-04-17 23:14:10 +0900

    Update app.js

--- accordion-anime/work/js/app.js
+++ accordion-anime/work/js/app.js
@@ -1,1 +1,16 @@
-const accordions = document.querySelectorAll('.list-item'); accordions.forEach(accordion => { accordion.addEventListener('click', () => { const content = accordion.querySelector('.list-content'); content.classList.toggle('active'); if(content.classList.contains('active')) { content.style.height = content.scrollHeight + 'px'; } else { content.style.height = '0px'; } }); });
+//ホバーで機能するアコーディオン機能にしました。アコーディオンの内容は、ホバーしたときに高さをscrollHeightにして、ホバーが外れたときに高さを0にすることで実装しています。
+const accordions = document.querySelectorAll('.list-item');
+
+accordions.forEach(accordion => {
+  const content = accordion.querySelector('.list-content');
+
+  accordion.addEventListener('mouseenter', () => {
+    content.classList.add('active');
+    content.style.height = content.scrollHeight + 'px';
+  });
+
+  accordion.addEventListener('mouseleave', () => {
+    content.classList.remove('active');
+    content.style.height = '0px';
+  });
+});
